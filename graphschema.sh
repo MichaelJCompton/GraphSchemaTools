cab4c0f42456014f412c3404e29d6ba14d7767b3
MichaelJCompton <MichaelJCompton@users.noreply.github.com>
2019-02-22 14:04:22 +1100

    update helper script

--- graphschema.sh
+++ graphschema.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 set -e
 
-docker run -p 10550:10550 mjcomp/graphschema "$@"
+if [ $1 == 'init-dgraph' ]; then
+    if [ "$#" -le 3 ]; then
+        docker run mjcomp/graphschema init-dgraph "--help"
+    else
+        folder="$(cd "$(dirname "$3")"; pwd -P)"
+        file="$(basename "$3")"
+        docker run --mount type=bind,source="$folder",destination=/data,readonly -p 10550:10550 mjcomp/graphschema init-dgraph "--schema-file" "/data/$file" "${@:4}"
+    fi
+else 
+    docker run -p 10550:10550 mjcomp/graphschema "$@"
+fi
